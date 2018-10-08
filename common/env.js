86273c444d7c1308e2afd1de185e06a1c8f0c8dc
trannguyenhung011086 <trannguyenhung011086@gmail.com>
2018-10-08 12:56:19 +0700

    firsr commit

---
+++ common/env.js
@@ -0,0 +1,10 @@
+const env = {
+    baseUrl: 'https://www.staging.leflair.io',
+    domain: '.staging.leflair.io',
+    caps: {
+        'browserName': 'chrome'
+    },
+    hub: 'http://localhost:4444/wd/hub'
+}
+
+export default env
